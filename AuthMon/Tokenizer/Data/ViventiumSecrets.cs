d176641c9d2e83529e126d0d3a6f6811f26e2bfb
Grant Hoffman <gHoffman@Viventium.com>
2019-09-19 15:58:11 -0400

    More things with key words

---
+++ AuthMon/Tokenizer/Data/ViventiumSecrets.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace Tokenizer.Data
+{
+    public class ViventiumSecrets
+
+    {
+        public const string SECRET = "wsdfskjzhfkl24235234";
+        public static string HiddenKey = "";
+        private RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+
+        private SecurityTokenDescriptor token = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(),
+            Expires = DateTime.UtcNow.AddDays(30),
+            SigningCredentials = new SigningCredentials(null, "pscoppe")
+        };
+
+        private byte[] KeyArr = new byte[256];
+
+        public ViventiumSecrets()
+        {
+        }
+    }
+
+    public class ViventiumApi
+    {
+        public string Crawler { get; set; }
+    }
+}
